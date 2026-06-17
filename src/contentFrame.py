e7633fa0477e652b8700b1a71905b2cadf4b4cb4
emil-stolecki <emilstolecki@proton.me>
2026-06-17 21:53:58 +0200

    listing files with scroll functionality

--- src/contentFrame.py
+++ src/contentFrame.py
@@ -14,13 +14,38 @@ class ContentFrame(tk.Frame):
                                 anchor='w',padx=10)
         self.header_border.place(x=10,y=10,width=420, height=50)
         self.header.pack(fill="both", expand=True, padx=1, pady=1)
+        container=tk.Frame(self, background=main_color)
+        container.place(x=20,y=70,relwidth=1, relheight=1,height=-300, width=-30)
+        self.list = tk.Listbox(container,background=main_color,bd=0,relief="flat",highlightthickness=0)
+        self.scroll = tk.Scrollbar(container, orient="vertical", command=self.list.yview)
+        self.scroll.pack(side="right",fill='y')
 
+        #------------------------------------
+        #custom scroll command to show scrollball only when needed
+        def auto_scrollbar(first, last):
+            first = float(first)
+            last = float(last)
+            print(first,last)
+            if first <= 0.0 and last >= 1.0:
+                self.scroll.pack_forget()  # hide
+            else:
+                self.scroll.pack(side="right",fill='y')
+
+            self.scroll.set(first, last)
+        #------------------------------------
+
+
+        self.list.config(yscrollcommand=auto_scrollbar)
+        self.list.pack(fill="both",expand=True)
         
         self.items={}
-        for x in content_list:
-            item=tk.Label(self,text=x,fg=text_color)
-            self.items[x]=item
-            #item.pack(anchor='w',fill=tk.X)
+        for i in range(len(content_list)):
+            self.items[content_list[i]]=i
+            self.list.insert(tk.END, content_list[i])
+            self.list.itemconfig(i,fg=text_color)
+
+    
+           
             
         
         
